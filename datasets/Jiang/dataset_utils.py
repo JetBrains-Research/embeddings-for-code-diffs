c7724d7e6968f8e7eb3452244297075f4060fbaf
Mikhail Pravilov <prav.mischa@gmail.com>
2020-01-30 19:12:20 +0300

    Add partitioning on neural editor and commit message generator.

--- datasets/Jiang/dataset_utils.py
+++ datasets/Jiang/dataset_utils.py
@@ -1,7 +1,7 @@
+import random
 import sys
-from pathlib import Path
-from typing import Tuple, Dict, List
 from distutils.dir_util import copy_tree
+from pathlib import Path
 
 from more_itertools import unzip
 
@@ -9,13 +9,49 @@ from datasets.Jiang.DatasetFilter import DatasetFilter
 from datasets.Jiang.GitDiffOutputProcessor import GitDiffOutputProcessor
 
 
-def partition_dataset(data_root):
-    pass
+def partition_dataset(data_root: Path) -> None:
+    new_data_root = data_root.joinpath('partitioned')
+    new_data_root.mkdir()
+    neural_editor_root = new_data_root.joinpath('neural_editor')
+    neural_editor_root.mkdir()
+    commit_message_generator_root = new_data_root.joinpath('commit_message_generator')
+    commit_message_generator_root.mkdir()
+    for data_dir in data_root.iterdir():
+        if data_dir.is_file() or data_dir.name == 'partitioned':
+            continue
+        neural_editor_dir = neural_editor_root.joinpath(data_dir.name)
+        neural_editor_dir.mkdir()
+        commit_message_generator_dir = commit_message_generator_root.joinpath(data_dir.name)
+        commit_message_generator_dir.mkdir()
+
+        diffs_file = data_dir.joinpath('diff.txt')
+        msg_file = data_dir.joinpath('msg.txt')
+        diffs_neural_editor = neural_editor_dir.joinpath('diff.txt')
+        msg_neural_editor = neural_editor_dir.joinpath('msg.txt')
+        diffs_commit_message_generator = commit_message_generator_dir.joinpath('diff.txt')
+        msg_commit_message_generator = commit_message_generator_dir.joinpath('msg.txt')
+
+        diffs = diffs_file.read_text().splitlines()
+        msgs = msg_file.read_text().splitlines()
+        assert len(diffs) == len(msgs)
+        dataset_size = len(diffs)
+        permutation = [i for i in range(dataset_size)]
+        random.shuffle(permutation)
+
+        division_id = int(len(permutation) / 2)
+        neural_editor_permutation = permutation[:division_id]
+        commit_message_generator_permutation = permutation[division_id:]
+
+        diffs_neural_editor.write_text('\n'.join([diffs[ind] for ind in neural_editor_permutation]))
+        msg_neural_editor.write_text('\n'.join([msgs[ind] for ind in neural_editor_permutation]))
+
+        diffs_commit_message_generator.write_text('\n'.join([diffs[ind] for ind in commit_message_generator_permutation]))
+        msg_commit_message_generator.write_text('\n'.join([msgs[ind] for ind in commit_message_generator_permutation]))
 
 
 def generate_and_save_prev_and_updated_versions(data_root: Path) -> None:
     for data_dir in data_root.iterdir():
-        if data_dir.is_file():
+        if data_dir.is_file() or data_dir.name == 'partitioned':
             continue
         diff_file = data_dir.joinpath('diff.txt')
         diffs = diff_file.read_text().splitlines()
