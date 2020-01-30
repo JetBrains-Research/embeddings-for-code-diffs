d6bf03ba18761098979040fe1775af64d5e3240a
Mikhail Pravilov <prav.mischa@gmail.com>
2020-01-30 16:57:56 +0300

    Remove unused methods.

--- datasets/Jiang/GitDiffOutputProcessor.py
+++ datasets/Jiang/GitDiffOutputProcessor.py
@@ -44,36 +44,12 @@ class GitDiffOutputProcessor:
                 prev_lines.append(tokens_in_line)
                 updated_lines.append(tokens_in_line)
 
-        #if tokens_per_line[0][0] == 'mmm':
-        #    prev_lines, updated_lines = GitDiffOutputProcessor.process_modification(tokens_per_line)
-        #elif tokens_per_line[0][0] == 'deleted':
-        #    prev_lines, updated_lines = GitDiffOutputProcessor.process_file_deletion(tokens_per_line)
-        #elif tokens_per_line[0][0] == 'new' and tokens_per_line[0][1] == 'file':
-        #    prev_lines, updated_lines = GitDiffOutputProcessor.process_file_addition(tokens_per_line)
-        #else:
-        #    raise Exception(f'Illegal diff:\n{git_diff_output}')
-
-        #prev_lines, updated_lines = \
-        #    GitDiffOutputProcessor.handle_additions_and_deletions(prev_lines, updated_lines)
         # TODO: leave <nl> in the end?
         prev = ' '.join(itertools.chain(*[line + ['<nl>'] for line in prev_lines]))
         updated = ' '.join(itertools.chain(*[line + ['<nl>'] for line in updated_lines]))
 
         assert (prev != updated)
         return prev, updated
-
-    @staticmethod
-    def handle_additions_and_deletions(prev_lines: List[List[str]],
-                                       updated_lines: List[List[str]]) -> Tuple[List[List[str]], List[List[str]]]:
-        return GitDiffOutputProcessor.handle_additions_and_deletions_prev_specified(prev_lines, is_prev=True), \
-               GitDiffOutputProcessor.handle_additions_and_deletions_prev_specified(updated_lines, is_prev=False)
-
-    @staticmethod
-    def handle_additions_and_deletions_prev_specified(tokens_per_line: List[List[str]], is_prev: bool) -> List[List[str]]:
-        sign_to_delete = '-' if is_prev else '+'
-        line_to_delete = ['ppp', '+'] if is_prev else ['mmm', '-']
-        return list(map(lambda x: x[1:] if x[0] == sign_to_delete else x,
-                        filter(lambda x: not x[0] in line_to_delete, tokens_per_line)))
 
     @staticmethod
     def get_prev_and_updated_for_diffs(git_diff_outputs: List[str]) -> List[Tuple[str, str]]:
@@ -84,25 +60,3 @@ class GitDiffOutputProcessor:
                 result.append(prev_and_updated)
         return result
 
-    @staticmethod
-    def process_modification(tokens_per_line: List[List[str]]) -> Tuple[List[List[str]], List[List[str]]]:
-        assert (tokens_per_line[0][0] == 'mmm')
-        assert (tokens_per_line[1][0] == 'ppp')
-
-        prev_lines = [tokens_per_line[0]] + tokens_per_line[2:]
-        updated_lines = [tokens_per_line[1]] + tokens_per_line[2:]
-        return prev_lines, updated_lines
-
-    @staticmethod
-    def process_file_deletion(tokens_per_line: List[List[str]]) -> Tuple[List[List[str]], List[List[str]]]:
-        start_index = 1
-        if tokens_per_line[1][0] == 'index':
-            start_index += 1
-        return tokens_per_line[start_index:], [tokens_per_line[0]] + tokens_per_line[start_index:]
-
-    @staticmethod
-    def process_file_addition(tokens_per_line: List[List[str]]) -> Tuple[List[List[str]], List[List[str]]]:
-        start_index = 1
-        if tokens_per_line[1][0] == 'index':
-            start_index += 1
-        return [tokens_per_line[0]] + tokens_per_line[start_index:], tokens_per_line[start_index:]
