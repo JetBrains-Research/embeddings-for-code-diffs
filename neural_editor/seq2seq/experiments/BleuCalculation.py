239b0f18938a70545bb59b7aa1ca11c9d85270a7
Mikhail Pravilov <prav.mischa@gmail.com>
2020-02-23 19:51:14 +0300

    Implement bleu score as float number retrieval.

--- neural_editor/seq2seq/experiments/BleuCalculation.py
+++ neural_editor/seq2seq/experiments/BleuCalculation.py
@@ -1,10 +1,18 @@
 import subprocess
 import tempfile
-from typing import List
+from typing import List, Tuple
 
 from torchtext.data import Dataset
 
 from neural_editor.seq2seq.config import Config
+
+
+def isfloat(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
 
 
 class BleuCalculation:
@@ -12,14 +20,31 @@ class BleuCalculation:
         super().__init__()
         self.config = config
 
-    def conduct(self, predictions: List[List[List[str]]], dataset: Dataset, dataset_label: str) -> None:
+    def get_bleu_script_output(self, predictions, dataset) -> Tuple[str, str]:
         top_1_predictions = ['' if len(prediction) == 0 else ' '.join(prediction[0]) for prediction in predictions]
         targets = [' '.join(example.trg) for example in dataset]
         with tempfile.NamedTemporaryFile(mode='w') as file_with_targets:
             file_with_targets.write('\n'.join(targets))
             file_with_targets.flush()
-            print(f'Start conducting BLEU calculation experiment for {dataset_label}...')
-            process = subprocess.Popen([self.config['BLEU_PERL_SCRIPT_PATH'], file_with_targets.name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process = subprocess.Popen([self.config['BLEU_PERL_SCRIPT_PATH'], file_with_targets.name],
+                                       stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             result = process.communicate(input=('\n'.join(top_1_predictions)).encode())
-            print(result[0])
+            return result
+
+    def conduct(self, predictions: List[List[List[str]]], dataset: Dataset, dataset_label: str) -> None:
+        print(f'Start conducting BLEU calculation experiment for {dataset_label}...')
+        result = self.get_bleu_script_output(predictions, dataset)
+        print(result[0])
+        print(f'Errors: {result[1]}')
+
+    def get_bleu_score(self, predictions: List[List[List[str]]], dataset: Dataset) -> float:
+        result = self.get_bleu_script_output(predictions, dataset)
+        print(result[0])
+        words = result[0].split()
+        if len(words) > 2 and len(words[2]) > 0 and isfloat(words[2][:-1]):
+            bleu_score = float(result[0].split()[2][:-1])
+            return bleu_score
+        else:
+            print('Warning: something wrong with bleu score')
             print(f'Errors: {result[1]}')
+            return 0
--- tests/datasets/Jiang/BleuCalculationTest.py
+++ tests/datasets/Jiang/BleuCalculationTest.py
@@ -68,6 +68,64 @@ class BleuChecking(unittest.TestCase):
         ]
         BleuChecking.BLEU_CALCULATION_EXPERIMENT.conduct(predictions, dataset, 'test')
 
+    def test_same_predicted_as_target_bleu_score(self):
+        predictions = [
+            [
+                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
+                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+            ],
+            [
+                'Fix typo'.split()
+            ]
+        ]
+        dataset = [
+            Example('Fix android_media_AudioSystem_getMasterMute return type .'.split()),
+            Example('Fix typo'.split())
+        ]
+        bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
+        self.assertEqual(100.0, bleu_score)
+
+    def test_bleu_zero_bleu_score(self):
+        predictions = [
+            [
+                'Completely incorrect guess .'.split(),
+                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+            ],
+            [
+                'Totally incorrect guess .'.split()
+            ]
+        ]
+        dataset = [
+            Example('Fix android_media_AudioSystem_getMasterMute return type .'.split()),
+            Example('Fix typo'.split())
+        ]
+        bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
+        self.assertEqual(0.0, bleu_score)
+
+    def test_only_one_example_bleu_score(self):
+        predictions = [
+            [
+                'Fix android_media_AudioSystem_getMasterMute return type .'.split(),
+                'Not fix android_media_AudioSystem_getMasterMute return type .'.split()
+            ]
+        ]
+        dataset = [
+            Example('Fix android_media_AudioSystem_getMasterMute return type .'.split())
+        ]
+        bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
+        self.assertEqual(100.0, bleu_score)
+
+    def test_no_predictions_bleu_score(self):
+        predictions = [
+            [], []
+        ]
+        dataset = [
+            Example('Fix android_media_AudioSystem_getMasterMute return type .'.split()),
+            Example('Fix typo'.split())
+        ]
+        bleu_score = BleuChecking.BLEU_CALCULATION_EXPERIMENT.get_bleu_score(predictions, dataset)
+        self.assertEqual(0.0, bleu_score)
+
 
 if __name__ == '__main__':
     unittest.main()
