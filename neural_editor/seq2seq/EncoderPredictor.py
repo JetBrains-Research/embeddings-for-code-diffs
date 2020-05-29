595ddd60181daaebb009136cc8633d14dbed238b
Mikhail Pravilov <prav.mischa@gmail.com>
2020-05-29 12:34:51 +0300

    Make predictor model 2 layer perceptron to be more powerful.

--- neural_editor/seq2seq/EncoderPredictor.py
+++ neural_editor/seq2seq/EncoderPredictor.py
@@ -18,13 +18,20 @@ class EncoderPredictor(nn.Module):
         super(EncoderPredictor, self).__init__()
         self.encoder = encoder
         self.edit_encoder = edit_encoder
-        self.bilinear = nn.Bilinear(encoder.hidden_size * 2, edit_encoder.hidden_size * 2, 1)
+        predictor_input_size = encoder.hidden_size * 2 + edit_encoder.hidden_size * 2
+        predictor_hidden_size = int(predictor_input_size / 2)
+        self.predictor_net = nn.Sequential(
+            nn.Linear(predictor_input_size, predictor_hidden_size),
+            nn.ReLU(),
+            nn.Linear(predictor_hidden_size, 1)
+        )
 
     def forward(self, batch: Batch) -> Tensor:
         (edit_hidden, _), _, (encoder_hidden, _) = self.encode(batch)
         encoder_hidden = encoder_hidden[-1]
         edit_hidden = edit_hidden[-1]
-        out = self.bilinear(encoder_hidden, edit_hidden)
+        predictor_input = torch.cat((encoder_hidden, edit_hidden), dim=-1)
+        out = self.predictor_net(predictor_input)
         return out.squeeze(dim=1)
 
     def predict(self, batch, labels=False) -> Tensor:
