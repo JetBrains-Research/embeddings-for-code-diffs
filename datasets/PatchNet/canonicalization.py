bf5e2ce2ba91a6f9d475a40b97c5e3a547c084a1
Mikhail Pravilov <prav.mischa@gmail.com>
2020-08-03 20:42:48 +0300

    Make canonicalization more verbose and add new token types.

--- datasets/PatchNet/canonicalization.py
+++ datasets/PatchNet/canonicalization.py
@@ -1,4 +1,6 @@
 import sys
+import time
+from datetime import timedelta
 from collections import defaultdict, Counter
 from pathlib import Path
 from typing import Dict, Any, Tuple, List
@@ -20,6 +22,7 @@ MIN_FREQ_THRESHOLDS = {
     Token.Name.Function: DEFAULT_MIN_FREQ,
     Token.Keyword.Reserved: DEFAULT_MIN_FREQ,
     Token.Literal.String.Escape: DEFAULT_MIN_FREQ,
+    Token.Literal.String.Affix: DEFAULT_MIN_FREQ,
     Token.Literal.Number.Integer: DEFAULT_MIN_FREQ,
     Token.Name.Label: DEFAULT_MIN_FREQ,
     Token.Literal.Number.Hex: DEFAULT_MIN_FREQ,
@@ -51,6 +54,11 @@ def get_tokens_to_leave(counter_by_types: Dict[Any, Counter]):
     new_dict = {}
     approximate_vocab_size = 0
     for token_type, counter in counter_by_types.items():
+        if token_type not in MIN_FREQ_THRESHOLDS:
+            print('Unknown token type!')
+            print(token_type)
+            print(counter)
+            exit(1)
         new_dict[token_type] = Counter({k: v for k, v in counter.items() if v >= MIN_FREQ_THRESHOLDS[token_type]})
         print(f'Size of {token_type} dict: {len(new_dict[token_type])}')
         approximate_vocab_size += len(new_dict[token_type])
@@ -59,9 +67,9 @@ def get_tokens_to_leave(counter_by_types: Dict[Any, Counter]):
 
 
 def is_text_allowed(token_text: str):
-    banned_substrings = [' ', '\n', '\r\n', '\r', '\t']
+    banned_substrings = ['\n', '\r\n', '\r', '\t']
     allowed_by_correct_splitting = ['ifdef CONFIG_SMP', 'ifndef __ASSEMBLY__', 'ifdef CONFIG_PM',
-                                    'ifdef CONFIG_OF', 'ifdef CONFIG_PM_SLEEP', 'GPL v2']
+                                    'ifdef CONFIG_OF', 'ifdef CONFIG_PM_SLEEP', 'GPL v2', '#if 0']
     if token_text in allowed_by_correct_splitting:
         return True
     for banned_substring in banned_substrings:
@@ -104,14 +112,26 @@ def canonicalize_sample(tokens_to_leave, prev_tokens, updated_tokens) -> \
 
 
 def canonicalize(tokens_to_leave: Dict[Any, Counter], dataset: PatchNetDataset):
+    vocab = set()
     lines_prev = []
     lines_updated = []
-    for sample in dataset.data_samples:
+    start = time.time()
+    for idx, sample in enumerate(dataset.data_samples):
         prev, updated = canonicalize_sample(tokens_to_leave, sample.commit.get_prev(), sample.commit.get_updated())
         lines_prev.append(' '.join(token[1] for token in prev))
         lines_updated.append(' '.join(token[1] for token in updated))
-    dataset.root.joinpath('canonicalized_prev.txt').write_text('\n'.join(lines_prev))
-    dataset.root.joinpath('canonicalized_updated.txt').write_text('\n'.join(lines_updated))
+        for token in lines_prev[-1].split() + lines_updated[-1].split():
+            vocab.add(token)
+
+        if (idx + 1) % 50000 == 0:
+            end = time.time()
+            duration = end - start
+            print(f'Processed {idx + 1} / {len(dataset.data_samples)} samples')
+            print(f'Time elapsed: {str(timedelta(seconds=duration))}', flush=True)
+            start = end
+    print(f'Final vocab size: {len(vocab)}')
+    dataset.root.joinpath(f'{DEFAULT_MIN_FREQ}_canonicalized_prev.txt').write_text('\n'.join(lines_prev))
+    dataset.root.joinpath(f'{DEFAULT_MIN_FREQ}_canonicalized_updated.txt').write_text('\n'.join(lines_updated))
 
 
 def perform_canonicalization():
@@ -123,7 +143,7 @@ def perform_canonicalization():
     patch_net_dataset.load()
     token_types_counter = sort_counter_by_token_types(patch_net_dataset.tokens_counter)
     tokens_to_leave = get_tokens_to_leave(token_types_counter)
-    print('Starting canonicalization!')
+    print('Starting canonicalization!', flush=True)
     canonicalize(tokens_to_leave, patch_net_dataset)
 
 
